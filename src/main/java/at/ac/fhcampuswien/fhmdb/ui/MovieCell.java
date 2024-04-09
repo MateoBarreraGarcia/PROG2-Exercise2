496a503b1f71985b869350064284803845b97623
MateoBarreraGarcia <mateo.barrera.garcia@stud.fh-campuswien.ac.at>
2024-04-09 22:19:38 +0200

    added release year and rating to moviecell layout, fixed tests from exercise 1 solution

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import java.util.stream.Collectors;
@@ -14,7 +15,10 @@ public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genre = new Label();
-    private final VBox layout = new VBox(title, detail, genre);
+    private final Label releaseYear = new Label();
+    private final Label rating = new Label();
+    private final HBox additionalInfos = new HBox(releaseYear, rating);
+    private final VBox layout = new VBox(title, detail, genre, additionalInfos);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -37,6 +41,8 @@ public class MovieCell extends ListCell<Movie> {
                     .map(Enum::toString)
                     .collect(Collectors.joining(", "));
             genre.setText(genres);
+            releaseYear.setText("Release Year:  " + movie.getReleaseYear());
+            rating.setText("Rating:  " + movie.getRating());
 
 
             // color scheme
@@ -44,12 +50,15 @@ public class MovieCell extends ListCell<Movie> {
             detail.getStyleClass().add("text-white");
             genre.getStyleClass().add("text-white");
             genre.setStyle("-fx-font-style: italic");
+            releaseYear.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
             detail.setMaxWidth(this.getScene().getWidth() - 30);
             detail.setWrapText(true);
+            releaseYear.setPadding(new Insets(0, 15, 0, 0));
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
