20cbabedb157f51e0832b0722ba31ce3c240aa6c
Naturdragon <63045626+Naturdragon@users.noreply.github.com>
2024-04-09 20:57:54 +0200

    Fixed error Filter

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -203,7 +203,8 @@ public class HomeController implements Initializable {
         Object genre = genreComboBox.getSelectionModel().getSelectedItem();
         Integer year = null;
         try {
-            if (yearComboBox.getEditor().getText() != "" && !yearComboBox.getEditor().getText().equals(yearFilerNoFilter))
+            String r = yearComboBox.getEditor().getText();
+            if (!yearComboBox.getEditor().getText().equals("") && !yearComboBox.getEditor().getText().equals(yearFilerNoFilter))
                 year = Integer.parseInt(yearComboBox.getEditor().getText());
         } catch (NumberFormatException e) {
             yearComboBox.setStyle("-fx-text-box-border: red;");
@@ -212,10 +213,11 @@ public class HomeController implements Initializable {
 
         Double rating = null; // Default value can be set because if nothing is set it is like 0.0 an above
         try {
-            if (ratingComboBox.getEditor().getText() != "" && !ratingComboBox.getEditor().getText().equals(ratingFilerNoFilter))
+            String r = ratingComboBox.getEditor().getText();
+            if (!ratingComboBox.getEditor().getText().equals("") && !ratingComboBox.getEditor().getText().equals(ratingFilerNoFilter))
                 rating = Double.parseDouble(ratingComboBox.getEditor().getText().replace("+",""));
         } catch (NumberFormatException e) {
-            yearComboBox.setStyle("-fx-text-box-border: red;");
+            ratingComboBox.setStyle("-fx-text-box-border: red;");
             return;
         }
 
